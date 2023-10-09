a7c99362ae536df8cd97f46b266a67884539fdbe
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-09 02:41:31 -0500

    Header component added to project

---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+const header = () => {
+  return (
+    <>
+      <header>
+        <h1>My App</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="some-id1">some link title 1</a>
+            </li>
+            <li>
+              <a href="some-id2">some link title 2</a>
+            </li>
+            <li>
+              <a href="some-id3">some link title 3</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default header;
