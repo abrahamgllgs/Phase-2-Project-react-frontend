cc5bddd81e489a83bd451e4f746dc1809de70376
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-09 03:04:50 -0500

    added footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+          <ul>
+            <li>
+              <a href="">Go to the top</a>
+            </li>
+            <li>
+              <a href="#2">social media link 1</a>
+            </li>
+            <li>
+              <a href="#">social media link 2</a>
+            </li>
+          </ul>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
