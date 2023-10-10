e9feaf994ca0f8d05c51037263da75f55d1b32b8
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-10 04:33:06 -0500

    Worked on my randomizer button, took a while

--- src/Components/RandomCocktail.jsx
+++ src/Components/RandomCocktail.jsx
@@ -1,8 +1,47 @@
 import { useState } from "react";
-import react from "@vitejs/plugin-react-swc";
 
-function RandomCocktailButton() {
+const RandomCocktailButton = () => {
   const [cocktail, setCocktail] = useState(null);
-}
 
-const getRandomCocktail = () => {};
+  const getRandomCocktail = () => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.drinks && data.drinks.length > 0) {
+          setCocktail(data.drinks[0]);
+        } else {
+          setCocktail(null);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setCocktail(null);
+      });
+  };
+
+  return (
+    <div>
+      <button
+        onClick={getRandomCocktail}
+        style={{ backgroundColor: "pink", color: "white" }}
+      >
+        Get a Cocktail
+      </button>
+      {cocktail && (
+        <div>
+          <h2 className="DrinkName">{cocktail.strDrink}</h2>
+          <p className="Instructions">{cocktail.strInstructions}</p>
+          <p className="Ingredient">{cocktail.strIngredient1}</p>
+          <p className="Ingredient">{cocktail.strIngredient2}</p>
+          <p className="Ingredient">{cocktail.strIngredient3}</p>
+          <p className="Ingredient">{cocktail.strIngredient4}</p>
+          <p className="Ingredient">{cocktail.strIngredient5}</p>
+          <p className="Ingredient">{cocktail.strIngredient6}</p>
+          <p className="Ingredient">{cocktail.strIngredient7}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RandomCocktailButton;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import RandomCocktailButton from "./Components/RandomCocktail.jsx";
+import "../src/styles.css";
+
+function cocktailButton() {
+  return (
+    <div>
+      <h1> Random Cocktail generator</h1>
+      <RandomCocktailButton /> {RandomCocktail}
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
