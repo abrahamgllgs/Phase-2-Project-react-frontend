aa9b213c84bbf8f55f99accae3652eb4d07b26c3
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-09 23:26:52 -0500

    added links to blog and github

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,17 +2,21 @@ const Footer = () => {
   return (
     <>
       <footer>
-          <ul>
-            <li>
-              <a href="">Go to the top</a>
-            </li>
-            <li>
-              <a href="#2">social media link 1</a>
-            </li>
-            <li>
-              <a href="#">social media link 2</a>
-            </li>
-          </ul>
+        <ul>
+          <li>
+            <a href="">Go to the top</a>
+          </li>
+          <li>
+            <a href="https://medium.com/@abrahamgllgs/html-vs-react-ac9ca549e897">
+              Blog Post
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/abrahamgllgs/Phase-2-Project-react-frontend">
+              GitHub link
+            </a>
+          </li>
+        </ul>
       </footer>
     </>
   );
