635c4323115345481e9c4ae449173c1c481f457b
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-10 04:31:46 -0500

    Added class names to work on style.css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,17 +2,18 @@ const header = () => {
   return (
     <>
       <header>
-        <h1>Cocktail App</h1>
+        <h1 className="AppName">Cocktail App</h1>
         <nav>
           <ul>
             <li>
-              <a href="some-id1">Random Cocktail</a>
+              <a href="some-id1" className="site-title">
+                Random Cocktail
+              </a>
             </li>
             <li>
-              <a href="some-id2">List of margaritas</a>
-            </li>
-            <li>
-              <a href="some-id3">some link title 3</a>
+              <a href="some-id2" className="site-list">
+                List of margaritas
+              </a>
             </li>
           </ul>
         </nav>
