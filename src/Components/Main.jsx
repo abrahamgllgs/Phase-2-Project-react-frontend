32850d99cb40d12be99b3c6e269402d9a182484e
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-09 23:27:44 -0500

    I added  my randomizer cocktail to main

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,10 +1,13 @@
 import MargaritasList from "./MargaritasList";
+import RandomCocktailButton from "./RandomCocktail";
 
 const Main = () => {
   return (
     <>
       <main>
         <h2>Main section </h2>
+        <h3>Randomizer</h3>
+        <RandomCocktailButton />
         <MargaritasList />
       </main>
     </>
