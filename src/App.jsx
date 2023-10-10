37ecb7501573628bd71c1f19c36ade290ace2265
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-10 14:48:25 -0500

    worked on my post form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
 import MargaritasList from "./Components/MargaritasList";
 import RandomCocktailButton from "./Components/RandomCocktail";
 import "../src/styles.css";
+import MyForm from "./Components/PostForm";
 
 const App = () => {
   const [count, setCount] = useState(0);
---
+++ src/Components/PostForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+function MyForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:5175/endpoint", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("POST response", data);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+export default MyForm;
